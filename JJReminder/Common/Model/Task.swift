c4f5ec499d1aa5f482c3d198d32fe4a363d1a3b6
jane-choi <jane.choi@navercorp.com>
2018-03-27 22:12:55 +0900

    [no-ticket] task 모델 업데이트

--- JJReminder/Common/Model/Task.swift
+++ JJReminder/Common/Model/Task.swift
@@ -12,10 +12,24 @@ class Task {
     var taskID: Int
     var title: String
     var status: Bool
+    var shouldAlarm: Bool
+    var alarmAt: Date
+    var createdAt: Date
+    var shouldRepeat: Bool
+    var priority: Int
+    var listId: Int
+    var memo: String
     
-    init(taskID: Int, title: String, status: Bool) {
+    init(taskID: Int, title: String, status: Bool, shouldAlarm: Bool, alarmAt: Date, createdAt: Date, shouldRepeat: Bool, priority: Int, listId: Int, memo: String) {
         self.taskID = taskID
         self.title = title
         self.status = status
+        self.shouldAlarm = shouldAlarm
+        self.alarmAt = alarmAt
+        self.createdAt = createdAt
+        self.shouldRepeat = shouldRepeat
+        self.priority = priority
+        self.listId = listId
+        self.memo = memo
     }
 }
